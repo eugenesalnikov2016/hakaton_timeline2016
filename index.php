f1237e003a74312b58a475c58731b938b31131e6
Tekiko <tekiko333@gmail.com>
2016-04-03 07:47:15 +0400

    index

---
+++ index.php
@@ -0,0 +1,151 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="index.css" rel="stylesheet">
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+		<!-- Добавление -->
+	<div class="plus">
+		<div class="vertical"></div>
+		<div class="horizontal"></div>
+	</div>
+	
+		<!-- Контент -->
+	<div class="content" style="background-image: url(images/title.png);
+								background-position: 50% 50%;
+								background-size: cover;">
+		<div class="contenttext">
+		
+				<h1>Рождение вселенной</h1>	
+				<p>По современным представлениям, наблюдаемая нами сейчас Вселенная возникла 13,7 млрд лет назад из некоторого начального сингулярного состояния и с тех пор непрерывно расширяется и охлаждается. В результате расширения и охлаждения во Вселенной произошли фазовые переходы, аналогичные конденсации жидкости из газа, но применительно к элементарным частицам.</p>
+			
+		</div>
+
+	</div>
+	<div class="content" style="background-image: url(images/title2.png);
+								background-position: 50% 50%;
+								background-size: cover;">
+		<div class="contenttext">
+		
+				<h1>Рождение вселенной</h1>	
+				<p>По современным представлениям, наблюдаемая нами сейчас Вселенная возникла 13,7 млрд лет назад из некоторого начального сингулярного состояния и с тех пор непрерывно расширяется и охлаждается. В результате расширения и охлаждения во Вселенной произошли фазовые переходы, аналогичные конденсации жидкости из газа, но применительно к элементарным частицам.</p>
+			
+		</div>
+		
+	</div>
+	<div class="content" style="background-image: url(images/title2.png);
+								background-position: 50% 50%;
+								background-size: cover;">
+		<div class="contenttext">
+		
+				<h1>Рождение вселенной</h1>	
+				<p>По современным представлениям, наблюдаемая нами сейчас Вселенная возникла 13,7 млрд лет назад из некоторого начального сингулярного состояния и с тех пор непрерывно расширяется и охлаждается. В результате расширения и охлаждения во Вселенной произошли фазовые переходы, аналогичные конденсации жидкости из газа, но применительно к элементарным частицам.</p>
+			
+		</div>
+		
+	</div>
+	<div class = "none"></div>
+	
+		<!-- НАВИГАЦИЯ -->
+
+	<div class="down">
+	
+		<div id="crumbs">
+			<ul>
+				<li><a href="#" >История времен</a></li>
+				<li><a href="#" >######</a></li>
+				<li><a href="#" >######</a></li>
+			</ul>
+		</div>
+	
+		<div class="timeline">
+
+				<table class="timelinetable">
+
+					<tr>
+
+						<td>
+							<div class="timelineblock" style="background-image: url(images/1.png);
+															background-position: 50% 50%;
+															background-size: cover; ">
+								<div class="text">13.7 - 4.8 млрд до н.э.</div>
+										
+							</div>
+						</td>
+						<td>
+							<div class="timelineblock" style="background-image: url(images/2.png);
+															background-position: 50% 50%;
+															background-size: cover; ">
+								<div class="text">4.8 - 4.6 млрд до н.э.</div>
+							</div>
+						</td>
+						<td>
+							<div class="timelineblock" style="background-image: url(images/3.png);
+															background-position: 50% 50%;
+															background-size: cover; ">
+								<div class="text">4.6 - 0.003 млрд до н.э.</div>
+							</div>
+						</td>
+						<td>
+							<div class="timelineblock" style="background-image: url(images/4.png);
+															background-position: 50% 50%;
+															background-size: cover; ">
+								<div class="text">3 млн - 5000 лет до н.э.</div>
+							</div>
+						</td>
+						<td>
+							<div class="timelineblock" style="background-image: url(images/5.png);
+															background-position: 50% 50%;
+															background-size: cover; ">
+								<div class="text">5000 - 0 лет до н.э.</div>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<div class="timelineblock" style="background-image: url(images/6.png);
+															background-position: 50% 50%;
+															background-size: cover; ">
+								<div class="text">0 - 500 лет н.э.</div>
+							</div>
+						</td>
+						<td>
+							<div class="timelineblock" style="background-image: url(images/7.png);
+															background-position: 50% 50%;
+															background-size: cover; ">
+								<div class="text">500 - 1000 лет н.э.</div>
+							</div>
+						</td>
+						<td>
+							<div class="timelineblock" style="background-image: url(images/8.png);
+															background-position: 50% 50%;
+															background-size: cover; ">
+								<div class="text">1000 - 1500 лет н.э.</div>
+							</div>
+						</td>
+						<td>
+							<div class="timelineblock" style="background-image: url(images/9.png);
+															background-position: 50% 50%;
+															background-size: cover; ">
+								<div class="text">1500 - 2000 лет н.э.</div>
+							</div>
+						</td>
+						<td>
+							<div class="timelineblock" style="background-image: url(images/10.png);
+															background-position: 50% 50%;
+															background-size: cover; ">
+								<div class="text">2000 - по наши дни</div>
+							</div>
+						</td>
+
+					</tr>
+					
+				</table>
+		</div>
+
+	</div>
+</body>
+</html>
