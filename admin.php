018d17d1af951d63413d0f3bc3b0324a38cc16e6
eugene <aliende2012@gmail.com>
2016-04-03 09:13:42 +0400

    7 commit

--- admin.php
+++ admin.php
@@ -29,8 +29,8 @@ if (!empty($_REQUEST)) {
         $status['event_name'] = 'Name is empty!';
         $success = false;
     }
-    if (empty($event_year) || !preg_match('/^[0-9]+$/', $event_year)) {
-        $status['event_year'] = 'Year is empty or containing letters!';
+    if (empty($event_year)) {
+        $status['event_year'] = 'Year is empty!';
         $success = false;
     }
     if (empty($event_text)) {
@@ -59,7 +59,7 @@ if (!empty($_REQUEST)) {
     if ($success) {
         $db = new DB;
 
-        if ($db->insert($event_name, $event_year, $event_text, $event_img_url, 'http://' .$event_video_url)) {
+        if ($db->insert($event_name, $event_year, $event_text, $event_img_url, 'https://' . $event_video_url)) {
             $result = 'Event successfully added';
         } else {
             $result = 'Error while addind to database';
@@ -77,132 +77,83 @@ if (!empty($_REQUEST)) {
 <head>
     <meta charset="UTF-8">
     <title>Admin Panel</title>
-    	    <link href="style.css" rel="stylesheet">
-	    <link href="foundation.css" rel="stylesheet">
-	    <script type="text/javascript" src="jquery.js"></script>
-	    <script type="text/javascript" src="foundation.min.js"></script
+    <link href="style.css" rel="stylesheet">
+    <link href="foundation.css" rel="stylesheet">
+    <script type="text/javascript" src="jquery.js"></script>
+    <script type="text/javascript" src="foundation.min.js"></script
 
 </head>
 <body>
-<h3>Add event</h3>
-<div>
+
+
+<div class="main">
+    <h2 class="text-center">Добавить событие</h2>
     <?= $result ?>
     <form name="add_event" action="" enctype="multipart/form-data" method="post">
-        <p>Name: <input name="event_name" type="text" value="<?= $event_name ?>"> <?= $status['event_name'] ?></p>
+        <!--
+                <div class="text-center">
 
-
-
-        <p>Year: <input name="event_year" type="text" value="<?= $event_year ?>"> <?= $status['event_year'] ?></p>
-        <p>Text: <input name="event_text" type="text" value="<?= $event_text ?>"> <?= $status['event_text'] ?></p>
-        <p>Image: <input name="event_img_url" type="file"> <?= $status['event_img_url'] ?>
-        </p>
-        <p>Video URL: http://<input name="event_video_url" type="text"
-                                    value="<?= $event_video_url ?>"> <?= $status['event_video_url'] ?></p>
-        <p><input name="submit" type="submit" value="Add"></p>
-    </form>
-</div>
-
-		<div class="main">
-		<h2 class="text-center">Добавить событие</h2>
-
-		<form method="POST" name="form1">
-		<div class="text-center">
-		    <input type="radio" name="times" id="pokemonRed"><label for="pokemonRed">млрд.</label>
+            <input type="radio" name="times" id="pokemonRed"><label for="pokemonRed">млрд.</label>
             <input type="radio" name="times" id="pokemonBlue"><label for="pokemonBlue">млн.</label>
             <input type="radio" name="times" id="pokemonBlue"><label for="pokemonBlue">По умолчанию</label>
             </div>
-		    
-		    <!--<p>Когда произошло событие</p>
-		    <p><label><input type="radio" name="veha">13,7 - 4,8 млрд. лет до н.э.</label> </p>
-		    <p><label><input type="radio" name="veha">4,8 - 4,6 млрд. лет до н.э.</label></p>
+            -->
 
-		    <p><label><input type="radio" name="veha">4,6 млрд. лет до н.э. - 3 млн. лет до н.э.</label></p> 
-		    <p><label><input type="radio" name="veha">3 млн. лет до н.э. - 5000 лет до н.э.</label></p>
 
-		    <p><label><input type="radio" name="veha">5000 лет до н.э. - Начало нашей эры</label></p> 
-		    <p><label><input type="radio" name="veha">Начало нашей эры - 500 г. н.э.</label></p>
-
-		    <p><label><input type="radio" name="veha">500г. - 1000г.</label></p> 
-		    <p><label><input type="radio" name="veha">1000г. - 1500г.</label class="right"></p>
-
-		    <p><label><input type="radio" name="veha">1500г. - 2000г.</label></p>
-		    <p><label><input type="radio" name="veha">2000г. - 2016г.</label></p>
-
-		    <select name="century">
-		    <option>1 век нашей эры</option>
-		    <option>2 век нашей эры</option>
-		    <option>3 век нашей эры</option>
-		    <option>4 век нашей эры</option>
-		    <option>5 век нашей эры</option>
-		    <option>6 век нашей эры</option>
-		    <option>7 век нашей эры</option>
-		    <option>8 век нашей эры</option>
-		    <option>9 век нашей эры</option>
-		    <option>10 век нашей эры</option>
-		    <option>11 век нашей эры</option>
-		    <option>12 век нашей эры</option>
-		    <option>13 век нашей эры</option>
-		    <option>14 век нашей эры</option>
-		    <option>15 век нашей эры</option>
-		    <option>16 век нашей эры</option>
-		    <option>17 век нашей эры</option>
-		    <option>18 век нашей эры</option>
-		    <option>19 век нашей эры</option>
-		    <option>20 век нашей эры</option>
-		    <option>21 век нашей эры</option>
-		    </select>-->
-		    <div class="row">
-                <div class="small-3 columns">
-                  <label for="middle-label" class="text-right middle">Год события</label>
-                </div>
-                <div class="small-9 columns">
-                  <input type="text" id="middle-label" name="year" required>
-                </div>
+        <div class="row">
+            <div class="small-3 columns">
+                <label for="middle-label" class="text-right middle">Год события</label>
             </div>
-            <div class="row">
-                <div class="small-3 columns">
-                  <label for="middle-label" class="text-right middle">Название</label>
-                </div>
-                <div class="small-9 columns">
-                  <input type="text" id="middle-label" name="nameEvent" required>
-                </div>
+            <div class="small-9 columns">
+                <input type="text" id="middle-label" value="<?= $event_year ?>" name="event_year"
+                       required><?= $status['event_year'] ?>
             </div>
-            <!--<p><label>Название события <input type="text" name="nameEvent" required></label></p>-->
-            <div class="row">
-                <div class="small-3 columns">
-                  <label for="middle-label" class="text-right middle">Oписание</label>
-                </div>
-                <div class="small-9 columns">
-                  <textarea name="description" rows="10" cols="15" id="middle-label" required></textarea>
-                </div>
-            </div>
-            <!--<p>Oписание события</p> 
-            <textarea name="description" rows="10" cols="15" required></textarea>
-            <p>-->
-            <div class="row">
-                <div class="small-3 columns">
-                  <label for="middle-label" class="text-right middle">Изображение</label>
-                </div>
-                <div class="small-9 columns">
-                  <label for="exampleFileUpload" class="button secondary">Обзор...</label><input type="file" accept="image/*" name="photo" id="exampleFileUpload" class="show-for-sr" required>
-                </div>
-            </div>
-            <!--<label for="exampleFileUpload" class="button secondary">Добавить изображение</label><input type="file" accept="image/*" name="photo" id="exampleFileUpload" class="show-for-sr" required></p>-->
-            <div class="row">
-                <div class="small-3 columns">
-                  <label for="middle-label" class="text-right middle">Ccылка на видео</label>
-                </div>
-                <div class="small-9 columns">
-                  <input type="text" name="video" required>
-                </div>
-            </div>
-            
-            <!--<p><label>Вставить ссылку на видео<input type="text" name="video" required></label></p>-->
-            <div class="text-center">
-            <input type="submit" name="butt" value="Отправить" class="button" id="middle-label">
-            </div>
-        </form>
         </div>
+        <div class="row">
+            <div class="small-3 columns">
+                <label for="middle-label" class="text-right middle">Название</label>
+            </div>
+            <div class="small-9 columns">
+                <input type="text" id="middle-label" name="event_name" value="<?= $event_name ?>"
+                       required><?= $status['event_name'] ?>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="small-3 columns">
+                <label for="middle-label" class="text-right middle">Oписание</label>
+            </div>
+            <div class="small-9 columns">
+                <textarea name="event_text" rows="10" cols="15" id="middle-label"
+                          required><?= $event_text ?></textarea><?= $status['event_text'] ?>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="small-3 columns">
+                <label for="middle-label" class="text-right middle">Изображение</label>
+            </div>
+            <div class="small-9 columns">
+                <label for="exampleFileUpload" class="button secondary">Обзор...</label>
+                <input type="file" accept="image/*" name="event_img_url" id="exampleFileUpload" class="show-for-sr"
+                       required><?= $status['event_img_url'] ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="small-3 columns">
+                <label for="middle-label" class="text-right middle">Ccылка на видео</label>
+            </div>
+            <div class="small-9 columns">
+                https://<input type="text" name="event_video_url" value="<?= $event_video_url ?>"
+                               required><?= $status['event_video_url'] ?>
+            </div>
+        </div>
+
+        <div class="text-center">
+            <input type="submit" name="submit" value="Отправить" class="button" id="middle-label">
+        </div>
+    </form>
+</div>
 
 </body>
 
--- index.php
+++ index.php
@@ -1,151 +1,151 @@
+<?php
+require_once 'DB.php';
+require_once 'Helper.php';
+
+
+$level = $_REQUEST['level'];
+$start = $_REQUEST['start'];
+$end = $_REQUEST['end'];
+if (empty($level)) {
+    $level = 0;
+}
+$tag = "a";
+if ($level > 1) {
+    $level = 1;
+    $tag = 'div';
+}
+if ($level == 0) {
+    $count = 10;
+} else {
+    $count = 5;
+}
+
+$years_count = $_REQUEST['end'] - $_REQUEST['start'];
+$section_count = ceil($years_count / $count);
+
+$db = new DB;
+$sql = 'SELECT * FROM `sections`';
+$result = mysqli_query($db->link, $sql);
+while ($row = mysqli_fetch_assoc($result)) {
+    $array[] = $row;
+}
+
+
+?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<link href="index.css" rel="stylesheet">
-	<meta charset="utf-8">
-	<title></title>
+    <link href="index.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <title></title>
 </head>
 <body>
-		<!-- Добавление -->
-	<div class="plus">
-		<div class="vertical"></div>
-		<div class="horizontal"></div>
-	</div>
-	
-		<!-- Контент -->
-	<div class="content" style="background-image: url(images/title.png);
+<!-- Добавление -->
+<div class="plus">
+
+    <a href="admin.php">
+        <div class="vertical"></div>
+        <div class="horizontal"></div>
+    </a>
+</div>
+
+<!-- Контент -->
+
+<?php
+if ($tag == 'div') {
+
+    $sql = "SELECT * FROM `events` WHERE `event_year` >= $start AND `event_year` <= $end ORDER BY `event_year`";
+
+    $result = mysqli_query($db->link, $sql);
+    while ($row = mysqli_fetch_assoc($result)) {
+        $events[] = $row;
+    }
+
+}
+?>
+
+
+
+<?php if (!empty($events)): ?>
+<?php foreach ($events as $event): ?>
+
+
+<div class="content" style="background-image: url(<?= $event['event_img_url'] ?>);
 								background-position: 50% 50%;
 								background-size: cover;">
-		<div class="contenttext">
-		
-				<h1>Рождение вселенной</h1>	
-				<p>По современным представлениям, наблюдаемая нами сейчас Вселенная возникла 13,7 млрд лет назад из некоторого начального сингулярного состояния и с тех пор непрерывно расширяется и охлаждается. В результате расширения и охлаждения во Вселенной произошли фазовые переходы, аналогичные конденсации жидкости из газа, но применительно к элементарным частицам.</p>
-			
-		</div>
+    <div class="contenttext">
 
-	</div>
-	<div class="content" style="background-image: url(images/title2.png);
-								background-position: 50% 50%;
-								background-size: cover;">
-		<div class="contenttext">
-		
-				<h1>Рождение вселенной</h1>	
-				<p>По современным представлениям, наблюдаемая нами сейчас Вселенная возникла 13,7 млрд лет назад из некоторого начального сингулярного состояния и с тех пор непрерывно расширяется и охлаждается. В результате расширения и охлаждения во Вселенной произошли фазовые переходы, аналогичные конденсации жидкости из газа, но применительно к элементарным частицам.</p>
-			
-		</div>
-		
-	</div>
-	<div class="content" style="background-image: url(images/title2.png);
-								background-position: 50% 50%;
-								background-size: cover;">
-		<div class="contenttext">
-		
-				<h1>Рождение вселенной</h1>	
-				<p>По современным представлениям, наблюдаемая нами сейчас Вселенная возникла 13,7 млрд лет назад из некоторого начального сингулярного состояния и с тех пор непрерывно расширяется и охлаждается. В результате расширения и охлаждения во Вселенной произошли фазовые переходы, аналогичные конденсации жидкости из газа, но применительно к элементарным частицам.</p>
-			
-		</div>
-		
-	</div>
-	<div class = "none"></div>
-	
-		<!-- НАВИГАЦИЯ -->
+        <h1><?= $event['event_name'] ?></h1>
+        <p><?= $event['event_text'] ?></p>
 
-	<div class="down">
-	
-		<div id="crumbs">
-			<ul>
-				<li><a href="#" >История времен</a></li>
-				<li><a href="#" >######</a></li>
-				<li><a href="#" >######</a></li>
-			</ul>
-		</div>
-	
-		<div class="timeline">
+    </div>
 
-				<table class="timelinetable">
+</div>
 
-					<tr>
+    <?php endforeach; ?>
+<?php endif; ?>
 
-						<td>
-							<div class="timelineblock" style="background-image: url(images/1.png);
-															background-position: 50% 50%;
-															background-size: cover; ">
-								<div class="text">13.7 - 4.8 млрд до н.э.</div>
-										
-							</div>
-						</td>
-						<td>
-							<div class="timelineblock" style="background-image: url(images/2.png);
-															background-position: 50% 50%;
-															background-size: cover; ">
-								<div class="text">4.8 - 4.6 млрд до н.э.</div>
-							</div>
-						</td>
-						<td>
-							<div class="timelineblock" style="background-image: url(images/3.png);
-															background-position: 50% 50%;
-															background-size: cover; ">
-								<div class="text">4.6 - 0.003 млрд до н.э.</div>
-							</div>
-						</td>
-						<td>
-							<div class="timelineblock" style="background-image: url(images/4.png);
-															background-position: 50% 50%;
-															background-size: cover; ">
-								<div class="text">3 млн - 5000 лет до н.э.</div>
-							</div>
-						</td>
-						<td>
-							<div class="timelineblock" style="background-image: url(images/5.png);
-															background-position: 50% 50%;
-															background-size: cover; ">
-								<div class="text">5000 - 0 лет до н.э.</div>
-							</div>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<div class="timelineblock" style="background-image: url(images/6.png);
-															background-position: 50% 50%;
-															background-size: cover; ">
-								<div class="text">0 - 500 лет н.э.</div>
-							</div>
-						</td>
-						<td>
-							<div class="timelineblock" style="background-image: url(images/7.png);
-															background-position: 50% 50%;
-															background-size: cover; ">
-								<div class="text">500 - 1000 лет н.э.</div>
-							</div>
-						</td>
-						<td>
-							<div class="timelineblock" style="background-image: url(images/8.png);
-															background-position: 50% 50%;
-															background-size: cover; ">
-								<div class="text">1000 - 1500 лет н.э.</div>
-							</div>
-						</td>
-						<td>
-							<div class="timelineblock" style="background-image: url(images/9.png);
-															background-position: 50% 50%;
-															background-size: cover; ">
-								<div class="text">1500 - 2000 лет н.э.</div>
-							</div>
-						</td>
-						<td>
-							<div class="timelineblock" style="background-image: url(images/10.png);
-															background-position: 50% 50%;
-															background-size: cover; ">
-								<div class="text">2000 - по наши дни</div>
-							</div>
-						</td>
 
-					</tr>
-					
-				</table>
-		</div>
+<div class="none"></div>
 
-	</div>
+<!-- НАВИГАЦИЯ -->
+
+<div class="down">
+
+    <div id="crumbs">
+        <ul>
+            <li><a href="index.php">История времен</a></li>
+
+        </ul>
+    </div>
+
+    <div class="timeline">
+
+        <table class="timelinetable">
+
+            <tr>
+                <?php if ($count == 10): ?>
+                    <?php for ($i = 0; $i < $count; $i++): ?>
+
+                        <td>
+                            <div class="timelineblock" style="background-image: url(images/<?= $i ?>.png);
+                                background-position: 50% 50%;
+                                background-size: cover; ">
+                                <div class="text"><a
+                                        href="?start=<?= $array[$i]['start'] ?>&end=<?= $array[$i]['end'] ?>&level=<?= ($level + 1) ?>"><?= $array[$i]['start'] . ' - ' . $array[$i]['end'] ?></a>
+                                </div>
+
+                            </div>
+                        </td>
+                        <?php if ($i == 4) echo '</tr><tr>'; ?>
+                    <?php endfor; ?>
+                <?php endif; ?>
+
+                <?php if ($count == 5): ?>
+                <?php for ($i = $start, $k = 0;
+                $i < $end;
+                $i = $i + $section_count, $k++): ?>
+                <td>
+                    <div class="timelineblock" style="background-image: url(images/<?= $k ?>.png);
+                        background-position: 50% 50%;
+                        background-size: cover; ">
+                        <div class="text">
+
+                            <<? echo ($tag == 'a') ? 'a href="?start=' . $i . '&end=' . ($i + $section_count) . '&level=' . ($level + 1) . '"' : 'div'; ?>
+                            ><?= $i . ' - ' . ($i + $section_count) ?> </<? echo ($tag == 'a') ? 'a' : 'div'; ?>
+                        >
+
+                    </div>
+    </div>
+    </td>
+    <?php endfor; ?>
+    <?php endif; ?>
+    </tr>
+
+    </table>
+</div>
+
+</div>
 </body>
 </html>
